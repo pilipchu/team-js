4932dc2caec12e0f610de267206411f09de7b3a8
Podolianyk <podolianik1988@gmail.com>
2024-01-14 18:32:24 +0100

    addjs

--- js/lesson-1(modal1).js
+++ js/lesson-1(modal1).js
@@ -1,1 +1,23 @@
-console.log("Hello world!!!!!!!!!!");
+//TODO: № 1 if...else ================================================
+// Якщо змінна a дорівнює 10, виведіть 'Вірно', інакше виведіть 'Невірно'.
+// const a = 5;
+// if (a===10) {
+//     console.log('Вірно');
+// } else {
+//     console.log('Невірно');
+// }
+
+// console.log(a === 10 ? "Вірно" : "Невірно");
+
+function getAnswer(a) {
+    if (a === 10) {
+        console.log('Вірно');
+    } else {
+        console.log('Невірно');
+    }
+}
+
+getAnswer(5);
+getAnswer(10);
+
+
