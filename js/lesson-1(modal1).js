4758f55efcb8e457ad4e6d00fbcabe4b8215af62
Ruslan-33 <newmiknet@gmail.com>
2024-01-14 20:41:08 +0200

    Ruslan

--- js/lesson-1(modal1).js
+++ js/lesson-1(modal1).js
@@ -27,11 +27,32 @@
 // показати через alert: "Вірно!"
 //інакше відобразити: "Не знаєте? ECMAScript!"
 
-const question = prompt("Яка офіційна назва JavaScript?");
-console.log("🚀 ~  question:", question);
-if (question==="ECMAScript") {
-  alert ("Вірно!")  
-} else {
-  alert("Не знаєте? ECMAScript!")  
-}
+// const question = prompt("Яка офіційна назва JavaScript?");
+// console.log("🚀 ~  question:", question);
+// if (question==="ECMAScript") {
+//   alert ("Вірно!")  
+// } else {
+//   alert("Не знаєте? ECMAScript!")  
+// }
 
+//TODO:  № 4   =============================================
+// Напишіть програму, яка отримає від користувача
+//число (кількість хвилин) і виведе у консоль
+//рядок у форматі годин і хвилин
+//70 === 01:10
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+
+
+// const time = 129;
+
+// const minuts = time % 60;
+// console.log(minuts);
+
+// const modifyMinuts = String(minuts).padStart(2, 0);
+ 
+// const hours = Math.floor(time / 60);
+// console.log(hours);
+
+// const modifyHours = String(hours).padStart(2, 0);
+// console.log(modifyHours);
+// console.log(`${modifyHours}:${modifyMinuts}`);
