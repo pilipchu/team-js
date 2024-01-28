dd2df57140ec922924d1368d2dd97baa800b04e8
Ruslan-33 <newmiknet@gmail.com>
2024-01-28 21:18:31 +0200

    Finish_lesson

--- js/lesson-2(2-3m).js
+++ js/lesson-2(2-3m).js
@@ -77,3 +77,25 @@
   
 // }
 // logItems(array)
+
+// /TODO: № 2 =============================================================
+//Напишіть функцію caclculateAverage()
+//яка приймає довільну кількість
+//аргументів і повертає їхнє середнє значення.
+//Додати перевірку, що аргументи це числа.
+
+function caclculateAverage(...args) {
+  // const args = arguments;
+
+  let sum = 0;
+  let total = 0;
+  for (const arg of args) {
+    if (typeof arg !== "number") {
+      continue;
+    }
+    sum += arg;
+    total += 1;
+  }
+  return sum / total;
+}
+console.log(caclculateAverage(1, 1, 2, 20, "word", "word2"));
