3c7b5d35417b059349eebfe434168e3d248cdee0
Ruslan-33 <newmiknet@gmail.com>
2024-02-25 20:10:24 +0200

    333

--- js/lesson-4(6-7).js
+++ js/lesson-4(6-7).js
@@ -1,11 +1,11 @@
 //TODO: № 1 на перебирання ✅ ==============================================
 //Знайти унікальні елементи за допомогою reduce
-const numbers = [1, 9, 0, 1, 5, 9, 1, 6];
+// const numbers = [1, 9, 0, 1, 5, 9, 1, 6];
 //Стрілочна функція
-const unique = (numbers) => 
-numbers.reduce((acc, number) => acc.includes(number) ? acc : [...acc, number], []);
+// const unique = (array) => 
+// array.reduce((acc, number) => acc.includes(number) ? acc : [...acc, number], []);
 
-console.log(unique(numbers));
+// console.log(unique(numbers));
 
 // //Класична функція
 // function unique(array) {
