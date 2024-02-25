f591eddc76d746ac65807d8b1e4013d90810fda6
Al14lex <alenahomenko1421@gmail.com>
2024-02-25 19:40:15 +0100

    123

--- js/lesson-4(6-7).js
+++ js/lesson-4(6-7).js
@@ -2,7 +2,7 @@
 //Знайти унікальні елементи за допомогою reduce
 // const numbers = [1, 9, 0, 1, 5, 9, 1, 6];
 //Стрілочна функція
-// const unique = (array) => 
+// const unique = (array) =>
 // array.reduce((acc, number) => acc.includes(number) ? acc : [...acc, number], []);
 
 // console.log(unique(numbers));
@@ -23,3 +23,12 @@
 //     }), [];
 //     return unique(numbers)
 // }
+
+//TODO: № 2 ==============================================
+// Натискання на кнопку "SHOW ME" має виводити значення в консоль з поля введення (дивіться на елементи в html-розмітці)
+/*
+const btn = document.querySelector("#alertButton")
+const inp = document.querySelector("#alertInput")
+
+btn.addEventListener("click", () => {console.log("жмякнули на кнопку", inp.value); })
+*/
